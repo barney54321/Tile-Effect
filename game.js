4e4369fdaa8deeba3ec2293679b5c540b9a72b5f
Andrew Esteban <andrewesteban1999@gmail.com>
2025-04-10 19:53:48 +1000

    Full screen attempt 2

--- game.js
+++ game.js
@@ -643,4 +643,13 @@ function setMenuColours() {
     })
 }
 
+function setFullHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+// Call it on load and resize
+window.addEventListener('load', setFullHeight);
+window.addEventListener('resize', setFullHeight);
+
 setMenuColours()
